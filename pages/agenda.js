e18eb4d6ed7f73b6fa1faefc382bcadf9a0ee77a
Kamille Konarzewski <kamille.kpimentel@gmail.com>
2021-09-02 09:58:56 -0300

    feat: date header

--- pages/agenda.js
+++ pages/agenda.js
@@ -1,28 +1,58 @@
-import {Button} from '@chakra-ui/react'
-import {useEffect} from 'react'
+import {useEffect, useState} from 'react'
 import {useRouter} from 'next/router'
-import {useAuth} from '../components'
+import {useFetch} from '@refetty/react'
+import axios from 'axios'
+import {addDays, subDays} from 'date-fns'
+
+import {ChevronLeftIcon, ChevronRightIcon} from '@chakra-ui/icons'
+import {Container, Box, Button, IconButton} from '@chakra-ui/react'
+
+import {useAuth, Logo, formatDate} from '../components'
+
+const getAgenda = (when) => axios({
+  method: 'get',
+  url: '/api/agenda',
+  params: {when},
+  /*headers: {
+    Authorization: `bearer ${token}`
+  }*/
+})
+
+const Header = ({children}) => (
+  <Box p={4} display='flex' alignItems='center' justifyContent='space-between'>
+    {children}
+  </Box>
+)
 
 export default function Agenda() {
-    const [auth, {logout}] = useAuth()
     const router = useRouter()
+    const [auth, {logout}] = useAuth()
+    const [when, setWhen]= useState(() => new Date())
+    const [data, {loading, status, error}, fetch] = useFetch(getAgenda, {lazy: true})
     
+    const addDay = () => setWhen(prevState => addDays(prevState, 1))
+    const removeDay = () => setWhen(prevState => subDays(prevState, 1))
+
     useEffect(() => {
       !auth.user && router.push('/')
     }, [auth.user])
 
+    useEffect(() => {
+      fetch(when)
+    }, [when])
+
     return (
-      <div>
-          <Button onClick={logout}>Sair</Button>
-      </div>
+      <Container>
+          <Header>
+            <Logo size={150}/>
+            <Button onClick={logout}>Sair</Button>
+          </Header>
+
+          <Box m={8} display='flex' alignItems='center' justifyContent='space-between'>
+            <IconButton icon={<ChevronLeftIcon />} bg='transparent' onClick={removeDay} />
+            <Box>{formatDate(when, 'PPPP')}</Box>
+            <IconButton  icon={<ChevronRightIcon />} bg='transparent' onClick={addDay} />
+          </Box>
+      </Container>
     )
   }
-
-/*export default function Agenda() {
-    return (
-      <div>
-          Agenda
-      </div>
-    )
-  }*/
-  

e088a7098ba8416961e3efbeb6446b1cc62f8c89
Kamille Konarzewski <kamille.kpimentel@gmail.com>
2021-09-12 15:23:36 -0300

    feat: persist schedule on database

--- pages/api/schedule.js
+++ pages/api/schedule.js
@@ -3,7 +3,7 @@ import {firebaseServer} from '../../config/firebase/server'
 import {addHours, differenceInHours, format} from 'date-fns'
 
 const db = firebaseServer.firestore()
-const profile = db.collection('profile')
+const profile = db.collection('profiles')
 const agenda = db.collection('agenda')
 
 const startAt = new Date(2021, 1, 1, 8, 0)
@@ -17,23 +17,54 @@ for(let blockIndex=0; blockIndex<=totalHours; blockIndex++) {
     timeBlocks.push(time)
 }
 
+const getUserId = async (username) => {
+    const profileDoc = await profile
+    .where('username', '==', username)
+    .get()
 
-export default async (req, res) => {
+    const {userId} = profileDoc.docs[0].data()
+
+    return userId
+}
+
+const setSchedule = async (req, res) => {
+    const userId = await getUserId(req.body.username)
+    const doc = await agenda.doc(`${userId}#${req.body.whe}`).get()
+
+    if(doc.exists){
+        return res.status(400)
+    }
+
+    agenda.doc(`${userId}#${req.body.when}`).set({
+        userId,
+        when: req.body.when,
+        name: req.body.name,
+        phone: req.body.phone
+    })
+
+    return res.status(200)
+}
+
+const getSchedule = (req, res) => {
     try {
-        /* const profileDoc = await profile
-            .where('username', '==', req.query.username)
-            .get()
-
-        const snapshot = await agenda
+        /* const snapshot = await agenda
             .where('user_id', '==', profileDoc.userId)
             .where('when', '==', req.query.when)
             .get()
- */
+        */
         return res.status(200).json(timeBlocks)
     }
     catch (error) {
         console.log('FB ERROR: ', error)
         return res.status(401)
     }
-
 }
+
+const methods = {
+    POST: setSchedule,
+    GET: getSchedule
+}
+
+export default async (req, res) => methods[req.method]
+    ? methods[req.method](req,res) 
+    : res.status(405)
